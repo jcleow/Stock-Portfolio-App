96b2a43ff2acd0bd017f52651e722013e0c3b9a1
jcleow <jc.leow94@gmail.com>
2021-02-07 23:39:22 +0800

    Try adding anonymous function

--- index.mjs
+++ index.mjs
@@ -24,20 +24,32 @@ app.use(express.static('dist'));
 // Set up Webpack in dev env
 const env = process.env.NODE_ENV || 'development';
 if (env === 'development') {
-  const { default: webpackDevMiddleware } =  await import('webpack-dev-middleware');
-  const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
-  const { default: webpackConfig } = await import('./webpack_conf/webpack.dev.js');
-  const compiler = webpack(webpackConfig);
-  app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    // html only
-    writeToDisk: (filePath) => /\.html$/.test(filePath),
-  }));
-  app.use(webpackHotMiddleware(compiler, {
-    log: false,
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000,
-  }));
+  // conditionally / dynamically import the webpack libraries we need in the dev environment
+  // a dynamic import syntax is called as a function and returns a promise
+  // use await so that every line is executed in order
+  // see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#dynamic_import
+
+  // destructure the default import and name the variable
+  // see more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#importing_defaults
+  (async () => {
+    const { default: webpack } = await import('webpack');
+    const { default: webpackDevMiddleware } = await import('webpack-dev-middleware');
+    const { default: webpackHotMiddleware } = await import('webpack-hot-middleware');
+    const { default: webpackConfig } = await import('./webpack_conf/webpack.dev.js');
+
+    const compiler = webpack(webpackConfig);
+
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+      // html only
+      writeToDisk: (filePath) => /\.html$/.test(filePath),
+    }));
+    app.use(webpackHotMiddleware(compiler, {
+      log: false,
+      path: '/__webpack_hmr',
+      heartbeat: 10 * 1000,
+    }));
+  });
 }
 
 // Bind route definitions to the Express application
